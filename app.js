f2c5ba37e6a15ebb15c73a888525fe99af5e9e20
rtukalenko <ruslan.tukalenko@gmail.com>
2019-04-10 23:36:33 +0300

    disable public

--- app.js
+++ app.js
@@ -50,8 +50,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-app.get('/favicon.ico', function (req, res) { res.status(204) });
 
 router.use('/roles', rolesRouter);
 router.use('/users', usersRouter);
