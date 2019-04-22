167a44b17b153ec2acfcb1134cea229dbf264e14
rtukalenko <ruslan.tukalenko@gmail.com>
2019-04-22 22:01:56 +0300

    remove domain

--- app.js
+++ app.js
@@ -38,9 +38,10 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
 
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,withCredentials,withcredentials');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-csrftoken');
+  // res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
