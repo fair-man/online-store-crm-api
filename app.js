b98bdeb491fdb8fa63eba5b0c4400897636282ed
rtukalenko <ruslan.tukalenko@gmail.com>
2019-04-22 23:03:37 +0300

    disabled origin prod for test

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ app.use(session({
 
 app.use(function (req, res, next) {
   var origins = [];
-  var origin = req.get('origin') || req.get('x-origin-domain');
+  var origin = req.get('origin');
 
-  if (process.env.ALLOW_ORIGIN_ADMIN_DEV) {origins.push(process.env.ALLOW_ORIGIN_ADMIN_DEV);}
+  // if (process.env.ALLOW_ORIGIN_ADMIN_DEV) {origins.push(process.env.ALLOW_ORIGIN_ADMIN_DEV);}
   // if (process.env.ALLOW_ORIGIN_ADMIN_PROD) {origins.push(process.env.ALLOW_ORIGIN_ADMIN_PROD);}
   // if (process.env.ALLOW_ORIGIN_WEB_DEV) {origins.push(process.env.ALLOW_ORIGIN_WEB_DEV);}
   // if (process.env.ALLOW_ORIGIN_WEB_PROD) {origins.push(process.env.ALLOW_ORIGIN_WEB_PROD);}
