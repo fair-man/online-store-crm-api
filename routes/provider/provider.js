aaf79546c99677b7f06e2f2b05a44c2f50cb8fd9
rtukalenko <ruslan.tukalenko@gmail.com>
2019-05-22 12:43:33 +0300

    add provider routes

--- routes/provider/provider.js
+++ routes/provider/provider.js
@@ -4,20 +4,44 @@ var db = require('../../libs/db-connect');
 var moment = require('moment');
 
 var responseFormatter = require('../../utils/responseFormatter');
+var requestValidator = require('../../utils/requestValidator');
+var providerSchemas = require('./providerSchema');
 
-router.post('/', function (req, res, next) {
-    var provider_json = req.body.provider_json;
-    provider_json.provider_data.created_date = moment().format('YYYY-MM-DD HH:mm:ss');
-    db.any('SELECT * from public.provider_create(${provider_json})', {provider_json: JSON.stringify(provider_json)})
-        .then(function (response) {
-            var opts = {data: {provider: response[0]}, rc: 0};
+router.post('/',
+    requestValidator.body(providerSchemas.providerCreateSchema),
+        function (req, res, next) {
+        var provider_json = req.body.provider_json;
+        provider_json.provider_data.created_date = moment().format('YYYY-MM-DD HH:mm:ss');
+        db.any('SELECT * from public.provider_create(${provider_json})', {provider_json: JSON.stringify(provider_json)})
+            .then(function (response) {
+                var opts = {data: {provider: response[0]}, rc: 0};
 
-            responseFormatter(200, opts, req, res);
-        }).catch(function (error) {
-        var opts = {error: error, rc: 500};
+                responseFormatter(200, opts, req, res);
+            }).catch(function (error) {
+            var opts = {error: error, rc: 500};
 
-        responseFormatter(500, opts, req, res);
+            responseFormatter(500, opts, req, res);
+        });
     });
-});
+
+router.put('/',
+    requestValidator.params(providerSchemas.providerParamsSchema),
+    requestValidator.body(providerSchemas.providerUpdateSchema),
+    function (req, res, next) {
+        var p_id = req.body.provider_json.provider_data.id;
+        var provider_json = req.body.provider_json;
+
+        db.any('SELECT * from public.provider_update(${p_id}, ${provider_json})',
+            {p_id: p_id, provider_json: JSON.stringify(provider_json)})
+            .then(function (response) {
+                var opts = {data: {provider: response[0]}, rc: 0};
+
+                responseFormatter(200, opts, req, res);
+            }).catch(function (error) {
+            var opts = {error: error, rc: 500};
+
+            responseFormatter(500, opts, req, res);
+        });
+    });
 
 module.exports = router;
--- routes/provider/providerSchema.js
+++ routes/provider/providerSchema.js
@@ -1,3 +1,99 @@
-/**
- * Created by rtukalenko on 16.05.2019.
- */
+var Joi = require('joi');
+
+var providerParamsSchema = {
+    p_id: Joi.string().required()
+};
+
+var providerCreateSchema = {
+    provider_json: {
+        provider_data: {
+            name: Joi.string().required().trim().min(2).max(60),
+            email: Joi.string().required().email()
+        },
+        provider_data_contract: {
+            c_number: Joi.number().required().min(2).max(60),
+            start_date: Joi.string().required(),
+            end_date: Joi.string().required()
+        },
+        provider_data_address_registration: {
+            region: Joi.string().required().min(3).max(60),
+            district: Joi.string().required().min(3).max(60),
+            city: Joi.string().required().min(3).max(60),
+            street: Joi.string().required().min(3).max(60),
+            street_type: Joi.number().required(),
+            building: [Joi.string().optional(), Joi.allow(null)],
+            house: Joi.string().required(),
+            flat: Joi.string(),
+            actual: Joi.number().required(),
+            registration: Joi.number().required()
+        },
+        provider_data_address_actual: {
+            region: Joi.string().required().min(3).max(60),
+            district: Joi.string().required().min(3).max(60),
+            city: Joi.string().required().min(3).max(60),
+            street: Joi.string().required().min(3).max(60),
+            street_type: Joi.number().required(),
+            building: [Joi.string().optional(), Joi.allow(null)],
+            house: Joi.string().required(),
+            flat: Joi.string(),
+            actual: Joi.number().required(),
+            registration: Joi.number().required()
+        },
+        provider_data_phones: Joi.array().items([{
+            phone: Joi.string().required().min(9),
+            type: Joi.number().required()
+        }])
+    }
+};
+
+var providerUpdateSchema = {
+    provider_json: {
+        provider_data: {
+            id: Joi.number().required(),
+            name: Joi.string().required().trim().min(2).max(60),
+            email: Joi.string().required().email()
+        },
+        provider_data_contract: {
+            id: Joi.number().required(),
+            c_number: Joi.number().required().min(2).max(60),
+            start_date: Joi.string().required(),
+            end_date: Joi.string().required()
+        },
+        provider_data_address_registration: {
+            id: Joi.number().required(),
+            region: Joi.string().required().min(3).max(60),
+            district: Joi.string().required().min(3).max(60),
+            city: Joi.string().required().min(3).max(60),
+            street: Joi.string().required().min(3).max(60),
+            street_type: Joi.number().required(),
+            building: [Joi.string().optional(), Joi.allow(null)],
+            house: Joi.string().required(),
+            flat: Joi.string(),
+            actual: Joi.number().required(),
+            registration: Joi.number().required()
+        },
+        provider_data_address_actual: {
+            id: Joi.number().required(),
+            region: Joi.string().required().min(3).max(60),
+            district: Joi.string().required().min(3).max(60),
+            city: Joi.string().required().min(3).max(60),
+            street: Joi.string().required().min(3).max(60),
+            street_type: Joi.number().required(),
+            building: [Joi.string().optional(), Joi.allow(null)],
+            house: Joi.string().required(),
+            flat: Joi.string(),
+            actual: Joi.number().required(),
+            registration: Joi.number().required()
+        },
+        provider_data_phones: Joi.array().items([{
+            phone: Joi.string().required().min(9),
+            type: Joi.number().required()
+        }])
+    }
+};
+
+module.exports = {
+    providerParamsSchema: providerParamsSchema,
+    providerCreateSchema: providerCreateSchema,
+    providerUpdateSchema: providerUpdateSchema
+};
