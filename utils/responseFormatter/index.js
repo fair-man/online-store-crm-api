030218b5520f6192d5d4c86c2d38a69ed6c72b5a
rtukalenko <ruslan.tukalenko@gmail.com>
2019-06-16 11:37:59 +0300

    small refactoring response formatter

--- utils/responseFormatter/index.js
+++ utils/responseFormatter/index.js
@@ -7,30 +7,10 @@ function responseFormatter(status, opts, req, res) {
     response_status.rc = res.status(status).type('application/json').send(JSON.stringify({
       data: opts.data, response_status: response_status
     }));
-  }
-
-  if (status === 404 || status === 500) {
-    res.status(status).type('application/json').send(JSON.stringify({
-      error: opts.error, response_status: response_status
-    }));
-  }
-
-  if (status === 400) {
-    res.status(status).type('application/json').send(JSON.stringify({
-      error: opts.error, response_status: response_status
-    }));
-  }
-
-  if (status === 401) {
-    res.status(status).type('application/json').send(JSON.stringify({
-      error: opts.error, response_status: response_status
-    }));
-  }
-
-  if (status === 403) {
-    res.status(status).type('application/json').send(JSON.stringify({
-      error: opts.error, response_status: response_status
-    }));
+  } else {
+      res.status(status).type('application/json').send(JSON.stringify({
+          error: opts.error, response_status: response_status
+      }));
   }
 }
 
