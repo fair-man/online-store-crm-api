459f1c3a468a2fb54658de0638a6ce621709cf36
rtukalenko <ruslan.tukalenko@gmail.com>
2019-06-16 11:35:10 +0300

    small refactoring auth middleware

--- utils/middleware/authentication.js
+++ utils/middleware/authentication.js
@@ -2,21 +2,15 @@ var responseFormatter = require('../responseFormatter/index');
 var Enums = require('../../config/enums/index');
 
 module.exports = function authorize(req, res, next) {
-  console.log('Проверка авторизации здесь');
-
-  if (req.originalUrl.indexOf('/auth/login') === -1 && req.originalUrl.indexOf('/auth/check_auth') === -1) {
+  if (['/auth/login', '/auth/check_auth'].indexOf(req.originalUrl) !== -1) {
     if (!req.session.user) {
-      var opts = {error: Enums.rcs[401], rc: 401};
-
-      responseFormatter(401, opts, req, res);
+      responseFormatter(401, {error: Enums.rcs[401], rc: 401}, req, res);
 
       return;
     }
 
     if (req.headers['x-csrftoken'] !== req.session.csrf_token) {
-      var opts = {error: 'token X-CSRFToken is missing', rc: 403};
-
-      responseFormatter(403, opts, req, res);
+      responseFormatter(403, {error: 'token is missing', rc: 403}, req, res);
 
       return;
     }
