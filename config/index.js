9433accc369b77ee474cff52831edf1efbdb2daf
Ruslan Tukalenko <ruslan.tukalenko@gmail.com>
2020-03-27 13:57:17 +0300

    test cors

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ nconf.set('mailer:host', process.env.EMAIL_HOST);
 nconf.set('mailer:port', process.env.EMAIL_HOST_PORT);
 nconf.set('mailer:secure', process.env.SECURE);
 nconf.set('session:secret', process.env.SESSION_SECRET_KEY);
-nconf.set('session:cookie:maxAge', new Date( Date.now() + 60 * 60 * 1000 ));
+// nconf.set('session:cookie:maxAge', new Date( Date.now() + 60 * 60 * 1000 ));
 nconf.set('cloudinary:cloudinary_name', process.env.CLOUDINARY_NAME);
 nconf.set('cloudinary:cloudinary_api_key', process.env.CLOUDINARY_API_KEY);
 nconf.set('cloudinary:cloudinary_api_secret', process.env.CLOUDINARY_API_SECRET);
